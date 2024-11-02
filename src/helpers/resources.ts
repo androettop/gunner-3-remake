04695e381d6d836995c0595c53c9a0539b9964e0
Pablo Androetto <androettop@gmail.com>
2024-11-02 14:28:01 +0100

    better loading screen

--- src/helpers/resources.ts
+++ src/helpers/resources.ts
@@ -1,9 +1,9 @@
-import { Loadable, Loader } from "excalibur";
+import { DefaultLoader, Loadable } from "excalibur";
 
 export const createLoader = (
   ...resourceObjects: Record<string, Loadable<any>>[]
 ) => {
-  const loader = new Loader();
+  const loader = new DefaultLoader();
   for (const resourceObject of resourceObjects) {
     for (const key in resourceObject) {
       loader.addResource(resourceObject[key]);
